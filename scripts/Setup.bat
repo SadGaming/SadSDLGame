249a0a027d1cc1077575e9164d31e94f3451a2bd
ASCIIBreakdown <td3of4@gmail.com>
2017-04-01 18:18:54 -0700

    Update Setup.bat

--- scripts/Setup.bat
+++ scripts/Setup.bat
@@ -3,6 +3,8 @@ if exist "SadSDLEngine" goto yes
 
 echo Engine not found. Downloading...
 git clone https://github.com/SadGaming/SadSDLEngine.git
+echo Moving files
+xcopy SadSDLEngine SadSDLGame\engine\ /E/Y
 echo Done
 goto end
 
@@ -11,6 +13,8 @@ echo Found Engine. Updating...
 cd SadSDLEngine/
 git pull
 cd ..
+echo Moving files
+xcopy SadSDLEngine SadSDLGame\engine\ /E/Y
 echo Done
 goto end
 
