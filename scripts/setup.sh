51885c9dddf67b3906f59ccfe0491d5a31f3a3bf
Adam Bryant <adam.w.bryant@outlook.com>
2017-04-17 17:37:14 +0000

    Improved shell script for setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+if [ -d "./engine" ]
+then
+    rmdir engine
+fi
+
+
+
 if [ -d "SadSDLEngine/" ]
 then
     echo "Found engine. Updating..."
     cd SadSDLEngine/
     git pull
-    ln -sr SadSDLEngine/ SadSDLGame/engine
+    
+    ln -sr . ../engine
     echo "Update Complete!"
 else
     echo "Engine not found. Downloading..."
     git clone https://github.com/SadGaming/SadSDLEngine.git
-    ln -sr SadSDLEngine/ SadSDLGame/engine
+    
+    ln -sr SadSDLEngine/ engine
     echo "Setup Complete"
 fi
